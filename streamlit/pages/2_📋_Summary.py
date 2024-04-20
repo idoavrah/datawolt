07c13139e232099a4215c42ae8130d455f222b9c
Ido Avraham <idoavraham@idf.il>
2024-04-20 17:09:07 +0300

    fix: fix top items computation

--- streamlit/pages/2_📋_Summary.py
+++ streamlit/pages/2_📋_Summary.py
@@ -23,20 +23,34 @@ with st.spinner("Loading summary data..."):
 
     order_totals = []
     dish_totals = {}
-    restaurant_totals = []
+    restaurant_totals = {}
 
     for user in orders_ref.stream():
         total_price = 0
         for order in user.get("orders"):
             total_price += order.get("total_price")
+            if order.get("venue_name") not in restaurant_totals:
+                restaurant_totals[order.get("venue_name")] = {
+                    "count": 1,
+                    "total_price": order.get("total_price"),
+                    "latitude": order.get("latitude"),
+                    "longitude": order.get("longitude"),
+                }
+            else:
+                restaurant_totals[order.get("venue_name")]["count"] += 1
+                restaurant_totals[order.get("venue_name")]["total_price"] += order.get(
+                    "total_price"
+                )
+
         for item in user.get("items"):
-            dish_totals[
-                (item.get("venue_name_fixed"), item.get("name"))
-            ] = dish_totals.get(
-                (item.get("venue_name_fixed"), item.get("name")), 0
-            ) + item.get(
-                "count"
-            )
+            if item.get("price") / item.get("count") > 14:
+                dish_totals[
+                    (item.get("venue_name_fixed"), item.get("name"))
+                ] = dish_totals.get(
+                    (item.get("venue_name_fixed"), item.get("name")), 0
+                ) + item.get(
+                    "count"
+                )
 
         if len(user.get("orders")) > 0:
             order_totals.append(
@@ -46,25 +60,27 @@ with st.spinner("Loading summary data..."):
                     "Average Order Price": round(total_price / len(user.get("orders"))),
                 }
             )
-
-    st.subheader("Expenses Scatter Chart")
-
-    st.scatter_chart(
-        order_totals,
-        x="Yearly Expense",
-        y="Num Orders",
-        size="Average Order Price",
-        color="Average Order Price",
-    )
-
     dish_totals = [
         {"venue_name_fixed": key[0], "name": key[1], "count": value}
         for key, value in dish_totals.items()
         if value > 10
     ]
+    restaurant_totals = dict(
+        sorted(
+            restaurant_totals.items(), key=lambda x: x[1]["total_price"], reverse=True
+        )[:20]
+    )
 
-    st.subheader("Top restaurants and items")
+    st.subheader(f"Expenses Scatter Chart ({len(order_totals)} data points).")
+    st.scatter_chart(
+        order_totals,
+        y="Yearly Expense",
+        x="Num Orders",
+        size="Average Order Price",
+        color="Average Order Price",
+    )
 
+    st.subheader("Top items (by number of orders)")
     chart = px.treemap(
         dish_totals,
         path=["venue_name_fixed", "name"],
@@ -72,8 +88,10 @@ with st.spinner("Loading summary data..."):
         color_continuous_scale="RdBu",
         color="venue_name_fixed",
     )
-
     chart.update_traces(texttemplate="%{value}<br>%{label}")
     chart.update_layout(margin=dict(t=0, l=0, r=0, b=0))
-
     chart
+
+    # st.subheader("Top 20 restaurants (by total expense)")
+    # locations = pd.DataFrame.from_dict(restaurant_totals, orient='index')
+    # st.table(locations)
